baf342c0a22fec60c64ab1fcc78b1cb8a194a404
Barry Xu <barry.xu@sony.com>
2020-06-03 10:55:29 +0800

    Add cache feature to optimize launching environment
    Use cache file to make 'source setup.bash' fast from second times
    of execution.
    
    Signed-off-by: Barry Xu <barry.xu@sony.com>

--- ament_package/template/prefix_level/setup.bash
+++ ament_package/template/prefix_level/setup.bash
@@ -4,8 +4,27 @@ AMENT_SHELL=bash
 
 # source setup.sh from same directory as this file
 AMENT_CURRENT_PREFIX=$(builtin cd "`dirname "${BASH_SOURCE[0]}"`" && pwd)
-# trace output
-if [ -n "$AMENT_TRACE_SETUP_FILES" ]; then
-  echo ". \"$AMENT_CURRENT_PREFIX/setup.sh\""
+
+ENV_CACHE_FILE="$HOME/.ros/env.cache"
+ENV_CACHE_LOCK_FILE="$HOME/.ros/.env.cache.lock"
+
+mkdir -p $(dirname "$ENV_CACHE_FILE")
+
+# If .env.cache.lock is locked, env.cache is being made. So cannot use it.
+if $(flock -n -x "$ENV_CACHE_LOCK_FILE" true) && [ -f "${ENV_CACHE_FILE}" ]; then
+  _cached_ordered_commands=$(cat "${ENV_CACHE_FILE}")
+  eval "$_cached_ordered_commands"
+  unset _ament_prefix_sh_source_script
+  echo "Use cached environmnet !"
+  echo "If you want to update cached environment, please remove \"${ENV_CACHE_FILE}\" !"
+  echo "And re-execute \". ${BASH_SOURCE[0]}\" in new terminal."
+else
+  # trace output
+  if [ -n "$AMENT_TRACE_SETUP_FILES" ]; then
+    echo ". \"$AMENT_CURRENT_PREFIX/setup.sh\""
+  fi
+  . "$AMENT_CURRENT_PREFIX/setup.sh"
 fi
-. "$AMENT_CURRENT_PREFIX/setup.sh"
+
+unset ENV_CACHE_FILE
+unset ENV_CACHE_LOCK_FILE
